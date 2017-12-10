06b7bcd26084565f74f800a60f863d93b9870550
Grayhamcracka77 <grayhamcracka77@gmail.com>
2017-12-09 20:40:34 -0700

    First commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    public Rigidbody rb;
+    public Vector3 com;
+
+    public GameObject bulletPrefab;
+    public float launcherFireRate = 0.5f;
+    public Transform rocketSpawn;
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float gunFireRate = 15f;
+    public Transform bulletSpawn;
+
+    private float nextTimeToFire = 0f;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(rb.centerOfMass);
+        rb.centerOfMass = com;
+        Debug.Log(rb.centerOfMass);
+    }
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / gunFireRate;
+            fireBullet();
+        }
+
+        if (Input.GetButtonDown("Fire2") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / launcherFireRate;
+            fireRocket();
+        }
+    }
+
+    void fireRocket()
+    {
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            rocketSpawn.position,
+            rocketSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 25;
+
+        Destroy(bullet, 2.0f);
+    }
+
+    void fireBullet()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rocketSpawn.transform.position, rocketSpawn.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    // Use this for initialization
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
