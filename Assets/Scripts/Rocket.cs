91183f2f22716fa338b49f2d1d1c5658a3a21686
bagelhobbit <evanturner45@gmail.com>
2017-12-13 11:24:54 -0700

    Rocket now explodes on contact.
    Explosion prefab is now rotated 180 degrees, unsure of the difference this made tbh.
    Changes to how/where explosion is instantiated, should be more consistent.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -13,12 +13,15 @@ public class Rocket : MonoBehaviour {
 
     void OnCollisionEnter (Collision col)
     {
+        if (col.collider.tag != "Player")
+        {
+            // Effect will start itself and destroy itself when finished so we don't need to destroy it
+            Instantiate(explosionPrefab, transform.position, transform.rotation);//Quaternion.AngleAxis(0f, explosionPrefab.transform.up));
+            Destroy (gameObject);
+        }
         if (col.collider.tag == "Target") 
         {
             col.collider.GetComponent<Target>().TakeDamage(rocketDamage);
-            // Effect will start itself and destroy itself when finished so we don't need to destroy it
-            Instantiate(explosionPrefab, col.transform.position, Quaternion.AngleAxis(180f, explosionPrefab.transform.right));
-            Destroy (gameObject);
         }
     }
 }
