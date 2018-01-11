a4b97e7792b9cb1aaa9b11480a2cb1fc75aa84c6
bagelhobbit <evanturner45@gmail.com>
2018-01-10 19:59:41 -0700

    Added CarEditor scene
    Made car part prefabs. Car will initialize on load. Car can be rotated.

---
+++ Assets/Scripts/LoadCarParts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCarParts : MonoBehaviour {
+
+    public Transform _carRoot;
+    public Transform _defaultCarBody;
+    public Transform _defaultWeapon;
+
+    public List<Transform> _alternativeWeapons;
+
+	void Start () {
+        Instantiate(_defaultCarBody, _carRoot);
+        Instantiate(_defaultWeapon, _carRoot);
+	}
+	
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/RotateCar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RotateCar : MonoBehaviour
+{
+    public GameObject carRoot;
+    public float speedH = 5.0f;
+    public float speedV = 5.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            yaw -= speedH * Input.GetAxis("Mouse X");
+            pitch += speedV * Input.GetAxis("Mouse Y");
+
+            carRoot.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+
+    }
+}
