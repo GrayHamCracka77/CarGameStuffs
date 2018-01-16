87bb34f94dfee584b682e92a973a2eede0a62d31
bagelhobbit <evanturner45@gmail.com>
2018-01-15 21:53:06 -0700

    Car body parts can be cycled with right arrow key

--- Assets/Scripts/LoadCarParts.cs
+++ Assets/Scripts/LoadCarParts.cs
@@ -2,19 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadCarParts : MonoBehaviour {
+public class LoadCarParts : MonoBehaviour
+{
 
     public Transform _carRoot;
     public Transform _defaultCarBody;
     public Transform _defaultWeapon;
 
-    public List<Transform> _alternativeWeapons;
+    public List<Transform> _possibleWeapons;
+    public List<Transform> _possibleCarBody;
 
-	void Start () {
-        Instantiate(_defaultCarBody, _carRoot);
+    private Transform currentBody;
+    private List<Transform>.Enumerator carBodyEnumerator;
+
+    void Start()
+    {
+        carBodyEnumerator = _possibleCarBody.GetEnumerator();
+        carBodyEnumerator.MoveNext();
+
+        currentBody = Instantiate(_defaultCarBody, _carRoot);
         Instantiate(_defaultWeapon, _carRoot);
-	}
-	
-	void Update () {
-	}
+    }
+
+    void Update()
+    {
+        Transform nextBody = null;
+        if (Input.GetKeyDown("right"))
+        {
+            var hasNext = carBodyEnumerator.MoveNext();
+            if (!hasNext)
+            {
+                carBodyEnumerator = _possibleCarBody.GetEnumerator();
+                carBodyEnumerator.MoveNext();
+            }
+            nextBody = carBodyEnumerator.Current;
+        }
+
+        if (nextBody != null)
+        {
+            Destroy(currentBody.gameObject);
+            currentBody = Instantiate(nextBody, currentBody.position, currentBody.rotation, _carRoot);
+        }
+    }
 }
